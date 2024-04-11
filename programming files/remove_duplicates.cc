21cf5bc0d59dbecc16e4416141e920e940eb78b4
BellaDavis <123829462+BellaDavis@users.noreply.github.com>
2024-04-11 10:34:24 -0400

    Update remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -8,11 +8,11 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     sort(arr.begin(), arr.end());
     int n = arr.size();
     for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] != arr[i-1]){
+        if(i < 0 || arr[i] != arr[i-1]){
             unique_arr.push_back(arr[i]);
         }
         else{
-            removed_elements.push_back(arr[i]);
+            removed_elements.push_back(arr(i));
         }
     }
     return unique_arr;
@@ -25,12 +25,12 @@ int main(){
     vector<int> unique_arr = remove_duplicates(arr, removed_elements);
     cout << "Original array: ";
     for(int i=0; i<arr.size(); i++){
-        cout << arr[i] << " ";
+        cout << arr(i) << " ";
     }
     cout << endl;
     cout << "Unique array: ";
     for(int i=0; i<unique_arr.size(); i++){
-        cout << unique_arr[i] << " ";
+        cout << unique_arr(i) << " ";
     }
     cout << endl;
     cout << "Removed elements: ";
